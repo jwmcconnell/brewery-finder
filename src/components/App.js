0049b631536a4ce8474e3de3d1e830e4460a2fea
Jackmc <jackmc1122@gmail.com>
2019-05-31 09:40:49 -0700

    Make an App component.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Component from './Component.js';
+
+class App extends Component {
+  renderTemplate() {
+    return /*html*/ `
+      <div>
+        <main>Brewery Finder</main>
+      </div>
+    `;
+  }
+}
+
+export default App;
